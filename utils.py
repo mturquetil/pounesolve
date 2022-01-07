a5c854c501fd90aeebe907b67b37b0a108523a8d
Maxime <maxime@mturquetil.fr>
2022-01-07 17:40:01 +0100

    feat: use binary class

--- utils.py
+++ utils.py
@@ -2,16 +2,15 @@ from logger import Logger
 import os
 
 def check_binary(binary):
-    print()
-    print(f'{Logger.custom("CHECKING FILE", style="bold")}: {Logger.custom(binary, color="blue", style="bold")}')
+    print(f'\n{Logger.custom("CHECKING FILE", style="bold")}: {Logger.custom(binary.path, color="blue", style="bold")}')
 
-    if binary.is_file():
+    if binary.posix_path.is_file():
         Logger.success('Path provided is a file')
     else:
         Logger.error('Path provided is not a file or does not exist')
         exit(1)
 
-    if os.access(binary, os.R_OK | os.X_OK):
+    if os.access(binary.path, os.R_OK | os.X_OK):
         Logger.success('Correct permissions')
     else:
         Logger.error('Read and execute permissions are needed')
